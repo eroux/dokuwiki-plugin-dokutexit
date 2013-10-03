eroux/dokuwiki-plugin-dokutexit

commit a860bda2080cb2cfed9e1a623c7dc091e730b103

Changing default to LuaLaTeX and no background

texitrender.php
@@ -368,9 +368,6 @@ class texitrender_plugin_texit {
 	$info .= '\\dokubackground{';
 	$info .= $latex->_latexEntities($hash['backgroundtext']) . "}\n";
       }
-    } else {
-      $info .= '\\dokubackground{'. $latex->_latexEntities(DOKU_URL) . "}\n";
-    } 
     foreach ( array_keys($this->_texit_conf) as $val ) {
       if (isset($hash[$val])) {
 	$this->_texit_conf[$val] = $hash[$val];

conf/default.php
@@ -23,5 +23,5 @@
 $conf['zipsources'] = false; // Show Download zip button
 $conf['dnl_button'] = true; // Show Download as PDF button
 $conf['force_clean_up'] = false; // Show Force Clean up button
-$conf['latex_mode'] = 'latex';
+$conf['latex_mode'] = 'lualatex';
 $conf['latex_path'] = '';

commit 9bea04219190c7f188bdd301be5f73663ab63927

Fix typo introduced in a860bda: missing if-statement end brace.

texitrender.php
@@ -368,6 +368,7 @@ class texitrender_plugin_texit {
 	$info .= '\\dokubackground{';
 	$info .= $latex->_latexEntities($hash['backgroundtext']) . "}\n";
       }
+    }
     foreach ( array_keys($this->_texit_conf) as $val ) {
       if (isset($hash[$val])) {
 	$this->_texit_conf[$val] = $hash[$val];